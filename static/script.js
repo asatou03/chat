ec1629754b2705cabaa128d034a0d97c7cc25216
asatou03 <157456810+asatou03@users.noreply.github.com>
2024-01-29 17:49:08 +0900

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,23 +1,7 @@
 const messageInput = document.getElementById("message-input");
-const postsList = document.querySelector("ul");
 
-const fetchAndRenderPosts = async () => {
-  try {
-    const response = await fetch('/');
-    const data = await response.text();
-
-    // 新しい要素を作成し、取得した HTML をその中に設定
-    const newElement = document.createElement("div");
-    newElement.innerHTML = data;
-
-    // 元の ul 要素に新しい要素を追加
-    postsList.innerHTML = '';
-    postsList.appendChild(newElement.querySelector("ul"));
-  } catch (error) {
-    console.error("Error fetching and rendering posts:", error);
+setInterval(() => {
+  if (messageInput.value === "") {
+    window.location.reload();
   }
-};
-
-setInterval(fetchAndRenderPosts, 5 * 1000);
-
-fetchAndRenderPosts();
+}, 5 * 1000);
