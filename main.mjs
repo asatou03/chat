f6cd71fc60ead8f344ada00fb6a54f3d60d6483c
asatou03 <157456810+asatou03@users.noreply.github.com>
2024-01-29 17:49:47 +0900

    Update main.mjs

--- main.mjs
+++ main.mjs
@@ -9,33 +9,20 @@ app.use(express.static("static"));
 const prisma = new PrismaClient();
 
 const template = fs.readFileSync("./template.html", "utf-8");
-
 app.get("/", async (request, response) => {
-  try {
-    const posts = await prisma.post.findMany();
-    const html = template.replace(
-      "<!-- posts -->",
-      posts.map((post, index) => `<li><strong>#${index + 1}</strong> ${escapeHTML(post.message)}</li>`).join(""),
-    );
-    response.send(html);
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    response.status(500).json({ error: "Internal Server Error" });
-  }
+  const posts = await prisma.post.findMany();
+  const html = template.replace(
+    "<!-- posts -->",
+    posts.map((post) => `<li>${escapeHTML(post.message)}</li>`).join(""),
+  );
+  response.send(html);
 });
 
 app.post("/send", async (request, response) => {
-  try {
-    await prisma.post.create({
-      data: { message: request.body.message },
-    });
-    response.redirect("/");
-  } catch (error) {
-    console.error("Error creating post:", error);
-    response.status(500).json({ error: "Internal Server Error" });
-  }
+  await prisma.post.create({
+    data: { message: request.body.message },
+  });
+  response.redirect("/");
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+app.listen(3000);
