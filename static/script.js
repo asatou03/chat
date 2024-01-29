ce82cf7ae286e5e4c9d11b81a338c91439aa97d7
asatou03 <157456810+asatou03@users.noreply.github.com>
2024-01-29 07:30:44 +0000

    番号修正

--- static/script.js
+++ static/script.js
@@ -7,6 +7,8 @@ const fetchAndRenderPosts = async () => {
     const response = await fetch('/');
     const data = await response.json();
 
+    console.log("Received data from server:", data);
+
     // サーバーサイドから取得したデータを元に番号つきのリストを構築
     postsList.innerHTML = data.map((post, index) => `<li><strong>#${index + 1}</strong> ${escapeHTML(post.message)}</li>`).join("");
   } catch (error) {
